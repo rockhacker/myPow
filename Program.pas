92a8fc252144682f83c8fae96baca48ee31ba7b2
rockhacker <rocky.rockhacker@gmail.com>
2024-07-02 19:41:09 +0800

    Describe your changes

--- Program.pas
+++ Program.pas
@@ -13,6 +13,7 @@ type
     class method Main(args: array of String);
     class method SHA256Hash(Input: String): String;
     class method FindPOW(Nickname: String): String;
+    class method FindPOW_50(Nickname: String): String;
   end;
 
 implementation
@@ -47,11 +48,32 @@ begin
   until false;
 end;
 
+class method Program.FindPOW_50(Nickname: String): String;
+begin
+  var nonce := 0;
+  var startTime := DateTime.Now;
+
+  repeat
+    var hash := SHA256Hash(Nickname + nonce.ToString);
+    if hash.StartsWith('00000') then
+    begin
+      var endTime := DateTime.Now;
+      var elapsedTime := (endTime - startTime).TotalSeconds;
+      exit String.Format('Nonce: {0}' + Environment.NewLine + 'Hash: {1}' + Environment.NewLine + 'Time: {2} seconds', [nonce, hash, elapsedTime]);
+    end;
+    inc(nonce);
+  until false;
+end;
+
 class method Program.Main(args: array of String);
 begin
   var nickname := 'rocky';
   var resultStr := FindPOW(nickname);
   Console.WriteLine(resultStr);
+
+  var resultStr_50 := FindPOW_50(nickname);
+  Console.WriteLine(resultStr_50);
+
 end;
 
 end.
